62a11565dfc9f5e20fd2ee0a977453835924d85f
Felipe Brito <felipe.brito87@gmail.com>
2022-02-15 12:08:42 -0300

    Inclusão dos áudios do jogo no projeto e criação dos objetos que os representam no logica.js

--- jogos-classicos-javascript/logica.js
+++ jogos-classicos-javascript/logica.js
@@ -6,6 +6,9 @@ import { Raquete } from './raquete.js';
 const mesa = new Mesa(600, 400, 'black');
 const placar = new Placar(0, 0, mesa.altura, mesa.largura);
 const bolinha = new Bolinha(300, 200, 10, 6, 6);
+const trilha = new Audio('./sons/trilha.mp3');
+const somPonto = new Audio('./sons/ponto.mp3');
+const somRaquetada = new Audio('./sons/raquetada.mp3');
 
 const raqueteJogador = new Raquete(5, 150, 10, 90, 30, mesa.altura);
 moveRaqueteJogador(raqueteJogador);
@@ -31,6 +34,7 @@ export function draw() {
     bolinha.desenha();
     raqueteJogador.desenha();
     raqueteOponente.desenha();
+    trilha.play();
 
     bolinha.move(mesa, raqueteJogador, raqueteOponente);
 
